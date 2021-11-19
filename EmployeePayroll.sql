5774fb696b17c631a3374fc8c413999440a1c050
swapnil-bagul <bagulswapnilp@gmail.com>
2021-11-19 14:21:21 +0530

    [Swapnil] Added UC2 Create table Employee Payroll

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -2,5 +2,12 @@
 create database Payroll_Services_Database
 use Payroll_Services_Database;
 
+---- UC2 Create table of employee payroll-----
+create table Employee_Payroll (
+Id int identity(1,1) primary key,
+Name varchar(200),
+Salary float,
+StartDate date
+);
 
 
